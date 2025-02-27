0149cc224a5817d4436537e58c33cd9683773f40
Ruben Glapa <rubenglapa@gmail.com>
2025-02-27 13:58:19 -0800

    First Commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        Contact
+    </x-slot:heading>
+    Hello from the Contact page
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-slot:heading>
+        Home
+    </x-slot:heading>
+    <div class="font-bold">
+        {{ $greeting }} from the Home page. My name is {{ $name }}</div>
+</x-layout>
